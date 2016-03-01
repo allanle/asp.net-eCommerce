dd0ed3793f354816ca0ea42d3049f03290d9daf2
allanle <ale11@my.bcit.ca>
2016-03-01 12:58:00 -0800

    Added CustomerRepository, ProductRespository, RepositoryBase, DataContext. Updated References of eCommerce.DAL

---
+++ eCommerce.DAL/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        // Pass the Name of a connection string or explicitly declare it
+        public DataContext() : base("DefaultConnectoin")
+        {
+
+        }
+
+        // Entity to be persisted must be declared here.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
