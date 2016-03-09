e4dc88d9761a1fb67f14deca51127d0d7cb9f9e6
allanle <ale11@my.bcit.ca>
2016-03-08 23:45:34 -0800

    Added Interfaces for IBasket.cs, IBasketItem.cs, IBasketVoucher.cs, IProduct.cs, iVoucher.cs, IVoucherType.cs

---
+++ eCommerce.Services/BasketService.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Contracts.Repositories;
+using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Services
+{
+    public class BasketService
+    {
+        IRepositoryBase<Basket> baskets;
+        private IRepositoryBase<Voucher> vouchers;
+        private IRepositoryBase<VoucherType> vourcherTypes;
+        private IReportsitoryBase<BasketVoucher> basketVouchers;
+
+        public const string BasketSessionName = "eCommerceBasket";
+
+        public BasketService(IRepositoryBase<Basket> baskets, IRepositoryBase<Voucher>, IRepositoryBase<BasketVoucher> basketVouchers, IRepositoryBase<VoucherType> voucherTypes)
+        {
+
+        }
+    }
+}
